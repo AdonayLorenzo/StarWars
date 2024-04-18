02e3fe3615925115a950f58de385c2c9033b6bfe
AdonayLorenzo <adonaylarempa@gmail.com>
2024-04-18 20:18:30 +0100

    Updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,16 @@
 import "./Header.css";
-
 function Header() {
   return (
     <>
       <div className="header">
-      <a> <img src='../../public/images/logo2.png' alt="logo" /></a>
-      <h1 id="header-text">Star Wars</h1>
+        <a href="/" className="logo"> <img src="logo3.png" alt="logo" className="logo" /></a>
+        <h1 id="header-text">Star Wars</h1>
+        <div className="Links">
+          <a href="/" className="header-links">Home</a>
+          <a href="/Privacity" className="header-links">Privacity</a>
+        </div>
       </div>
     </>
- )
+  )
 }
- export default Header;
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,19 +11,38 @@ const Home = () => {
         <h1>Welcome to Star Wars</h1>
         <p>Explore the universe of Star Wars and discover all the information you need to know about your favorite characters, naves espaciales, planetas, etc.</p>
         <br></br>
-        <br/>
+        <br />
         <p>A website created with React to learn about this JavaScript framework and as a work of the Markup Languages subject. Explore the universe of Star Wars and discover all you need to know about your characters, spaceships, planets, etc.</p>
         <div className="links">
+          <h2>Visit the other Pages</h2>
           <ul>
             <li>
-              <a href="/Privacity">Privacity</a>
+              <a href="/Privacity" className='links'>Privacity</a>
             </li>
             <li>
-              <a href="/Gallery">Gallery</a>
+              <a href="/Gallery" className='links'>Gallery</a>
             </li>
           </ul>
         </div>
+        <br></br>
+        <br></br>
       </div>
+      <h2 className='title'>Characters</h2>
+      <br/>
+      <div className="characters">
+          <div className="character">
+            <h2>Luke Skywalker</h2>
+            <p>Luke Skywalker is a fictional character and the main protagonist of the original film trilogy of the Star Wars franchise.</p>
+          </div>
+          <div className="character">
+            <h2>Han Solo</h2>
+            <p>Han Solo is a fictional character in the space opera franchise Star Wars.</p>
+          </div>
+          <div className="character">
+            <h2>Princesa Leia</h2>
+            <p>Princess Leia Organa is a fictional character in the Star Wars franchise, portrayed in films by Carrie Fisher.</p>
+          </div>
+        </div>
       <Footer />
     </div>
   );
